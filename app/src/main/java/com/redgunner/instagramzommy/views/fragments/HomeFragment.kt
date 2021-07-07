f745560b65d430fb4f6bb1bb69939cf5d5780f49
Youssef El Mountahi <yusuf.one@outlook.com>
2021-07-07 19:26:57 +0100

    Add exit ad

--- app/src/main/java/com/redgunner/instagramzommy/views/fragments/HomeFragment.kt
+++ app/src/main/java/com/redgunner/instagramzommy/views/fragments/HomeFragment.kt
@@ -2,11 +2,14 @@ package com.redgunner.instagramzommy.views.fragments
 
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
 import com.redgunner.instagramzommy.R
@@ -20,6 +23,8 @@ class HomeFragment : Fragment(R.layout.home_fragment) {
 
 
     private val viewModel: SharedViewModel by activityViewModels()
+    private lateinit var mInterstitialAd: com.google.android.gms.ads.InterstitialAd
+
 
     private val accountListAdapter = SearchInstagramUserListAdapter { account ->
         viewModel.addAccountToHistory(account)
@@ -32,6 +37,7 @@ class HomeFragment : Fragment(R.layout.home_fragment) {
     override fun onStart() {
         super.onStart()
         setUpAds()
+        setUpInterstitialAd()
         setUpRecyclerView()
         setUpObservers()
 
@@ -40,6 +46,14 @@ class HomeFragment : Fragment(R.layout.home_fragment) {
 
     override fun onResume() {
         super.onResume()
+
+        mInterstitialAd.adListener= object : AdListener() {
+
+            override fun onAdClosed() {
+                super.onAdClosed()
+                requireActivity().finish()
+            }
+        }
 
 
         Input.addTextChangedListener(object : TextWatcher {
@@ -88,6 +102,20 @@ class HomeFragment : Fragment(R.layout.home_fragment) {
 
             }
         })
+
+
+        val callback = requireActivity().onBackPressedDispatcher.addCallback(this) {
+
+
+
+            if (mInterstitialAd.isLoaded){
+                mInterstitialAd.show()
+            }else{
+                requireActivity().finish()
+            }
+
+        }
+
 
     }
 
@@ -148,6 +176,17 @@ class HomeFragment : Fragment(R.layout.home_fragment) {
         val adRequest = AdRequest.Builder().build()
         adView.loadAd(adRequest)
     }
+
+    private fun setUpInterstitialAd() {
+        mInterstitialAd = com.google.android.gms.ads.InterstitialAd(this.context)
+
+        mInterstitialAd.adUnitId = getString(R.string.exitInterstitialAdID)
+        mInterstitialAd.loadAd(AdRequest.Builder().build())
+
+    }
+
+
+
 
 
 }
